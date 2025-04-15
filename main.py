76a63692168668841b7a7feebee6c0e67d2d45d7
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-04-15 14:13:02 +0200

    Save the end result to an .xlsx instead

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ import pandas as pd
 
 # File paths
 input_file = 'data/all_offers.csv'
-output_file = 'end_data/all_offers_ready.csv'
+output_file = 'end_data/all_offers_ready.xlsx'
+
+print('Reading the input file...')
 
 df = pd.read_csv(input_file, delimiter=';')
 mask = df['name'].str.contains('OUTLET: ', case=False)
 
+print('Creating the copy with cleaned descriptions...')
+
 all_outlets = df[mask].copy()
 all_outlets['new_description'] = all_outlets['description'].apply(clean_html)
 
-all_outlets.to_csv(output_file, sep=';')
+print(f'Saving {len(all_outlets)} to an excel file')
+
+all_outlets.to_excel(output_file)
+
+print('Finished!')
