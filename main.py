b49ccc2a35122f052d56efc25070c6781c041f49
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-04-15 14:00:26 +0200

    Add Pandas and Openpyxl

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 import os
 from functions import clean_html
+import pandas as pd
 
 # File paths
-input_path = 'data/8809971229227.html'
-output_path = 'end_data/8809971229227.html'
+input_file = 'data/all_offers.csv'
+output_file = 'end_data/all_offers_ready.csv'
 
-
-
-with open(input_path, 'r', encoding='utf-8') as f:
-    raw_html = f.read()
-
-cleaned_body = clean_html(raw_html)
-final_html = cleaned_body
-
-os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
-with open(output_path, 'w', encoding='utf-8') as f:
-    f.write(final_html)
+final_html = clean_html(product_description)
